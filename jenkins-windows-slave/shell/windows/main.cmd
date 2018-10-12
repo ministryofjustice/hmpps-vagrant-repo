a1a05f76fb1222b1f0279c3ee7861a6851da9199
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-12 19:09:59 +0100

    Not install ansible

--- jenkins-windows-slave/shell/windows/main.cmd
+++ jenkins-windows-slave/shell/windows/main.cmd
@@ -4,6 +4,9 @@ echo *****************Executing main script ****************
 
 @powershell -NoProfile -ExecutionPolicy Bypass -File "%systemdrive%\vagrant\shell\windows\InstallChocolatey.ps1"
 
-@powershell -NoProfile -ExecutionPolicy Bypass -File "%systemdrive%\vagrant\shell\windows\InstallPytonPackages.ps1"
+refreshenv
+
+echo *** Installing ansible ***
+pip install ansible
 
 echo "*****************WINDOWS VM PREP COMPLETE****************"
