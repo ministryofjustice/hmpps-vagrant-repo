bcd77fab66275be5541234506b5938dec518b294
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-15 19:47:56 +0100

    swarm client download v6

--- jenkins-windows-slave/shell/windows/InstallChocolatey.ps1
+++ jenkins-windows-slave/shell/windows/InstallChocolatey.ps1
@@ -28,5 +28,10 @@ New-Item -Path c:\jenkins\workspace -ItemType directory
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 wget -OutFile c:\jenkins\swarm-client.jar https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/3.14/swarm-client-3.14.jar
 
+#---- Add swarm service ---
+New-Service -Name "JenkinsSwarm" -BinaryPathName "java -jar c:\jenkins\swarm-client.jar -master http://10.168.253.54:8080 -username admin -password admin -name jenkins-swarm-windows-slave-1"
+Set-Service -Name "JenkinsSwarm" -StartupType Automatic
+Start-Service -Name "JenkinsSwarm"
+
 #--- Restore Temporary Settings ---
 Enable-UAC
