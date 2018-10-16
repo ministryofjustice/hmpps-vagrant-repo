03b485739bda9b4801df5fce1099772559892e59
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-16 12:31:30 +0100

    salt dev 3

---
+++ jenkins-windows-slave/shell/windows/bootstrap.ps1
@@ -0,0 +1,35 @@
+$ChocoInstallPath = "$env:SystemDrive\ProgramData\Chocolatey\bin"
+
+if (!(Test-Path $ChocoInstallPath)) {
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+#---- TEMPORARY ---
+Disable-UAC
+
+#--- Python ---
+choco install python2 -y
+
+#--- wget ---
+choco install wget -y
+
+#--- Salt Minion ----
+choco install saltminion -y
+
+cd c:\salt
+
+salt-call --local chocolatey.install git.install
+
+#--- Create jenkins dir
+New-Item -Path c:\jenkins\workspace -ItemType directory
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+wget -OutFile c:\jenkins\swarm-client.jar https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/3.14/swarm-client-3.14.jar
+
+# #---- Add swarm service ---
+# New-Service -Name "JenkinsSwarm" -BinaryPathName "java -jar c:\jenkins\swarm-client.jar -master http://10.168.253.54:8080 -username admin -password admin -name jenkins-swarm-windows-slave-1"
+# Set-Service -Name "JenkinsSwarm" -StartupType Automatic
+# Start-Service -Name "JenkinsSwarm"
+
+#--- Restore Temporary Settings ---
+Enable-UAC
--- jenkins-windows-slave/shell/windows/main.cmd
+++ jenkins-windows-slave/shell/windows/main.cmd
@@ -2,14 +2,10 @@ echo *****************STARTING WINDOWS PREP ****************
 
 echo *****************Executing main script ****************
 
-@powershell -NoProfile -ExecutionPolicy Bypass -File "%systemdrive%\vagrant\shell\windows\InstallChocolatey.ps1"
+@powershell -NoProfile -ExecutionPolicy Bypass -File "%systemdrive%\vagrant\shell\windows\bootstrap.ps1"
 
 refreshenv
 
 REM java -jar c:\jenkins\swarm-client.jar -master http://10.168.253.54:8080 -username admin -password admin -name jenkins-swarm-windows-slave-1
-
-cd c:\salt
-
-salt-call --local chocolatey.install git.install
 
 echo "*****************WINDOWS VM PREP COMPLETE****************"
