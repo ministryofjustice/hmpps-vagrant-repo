5d8407d6a77d5135254df8cc60f75649795ef0dd
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-16 14:49:57 +0100

    salt dev 8

--- jenkins-windows-slave/shell/windows/bootstrap.ps1
+++ jenkins-windows-slave/shell/windows/bootstrap.ps1
@@ -5,7 +5,7 @@ refreshenv
 
 Set-Location -Path c:\salt
 
-Start-Process "cmd.exe" "/c salt-call --local chocolatey.install git.install"  -Verb runAs
+Start-Process "cmd.exe" "/c salt-call --local chocolatey.install git.install"
 
 #--- Restore Temporary Settings ---
 Enable-UAC
