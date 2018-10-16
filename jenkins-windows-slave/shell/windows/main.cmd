feeae47227656bf5c2d975d15935147bd5b35434
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-16 12:22:59 +0100

    salt dev 2

--- jenkins-windows-slave/shell/windows/main.cmd
+++ jenkins-windows-slave/shell/windows/main.cmd
@@ -8,6 +8,8 @@ refreshenv
 
 REM java -jar c:\jenkins\swarm-client.jar -master http://10.168.253.54:8080 -username admin -password admin -name jenkins-swarm-windows-slave-1
 
+cd c:\salt
+
 salt-call --local chocolatey.install git.install
 
 echo "*****************WINDOWS VM PREP COMPLETE****************"
