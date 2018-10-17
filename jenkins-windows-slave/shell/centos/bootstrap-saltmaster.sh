0be5dfcd1f98d20a4c592339291c68b96e100742
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-17 12:42:02 +0100

    Added bootstrap folder v13

--- jenkins-windows-slave/shell/centos/bootstrap-saltmaster.sh
+++ jenkins-windows-slave/shell/centos/bootstrap-saltmaster.sh
@@ -4,7 +4,16 @@ echo '### SALTMASTER SETUP'
 
 yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm -y
 
-yum install -y salt-master salt-minion
+yum install -y salt-master salt-minion git
 
 systemctl enable salt-master salt-minion
 systemctl start salt-master salt-minion
+
+yum install -y epel-release
+
+yum install python-pygit2 -y
+
+salt-run winrepo.genrepo
+salt-run winrepo.update_git_repos
+
+salt -G 'os:windows' pkg.refresh_db
--- jenkins-windows-slave/shell/windows/bootstrap.cmd
+++ jenkins-windows-slave/shell/windows/bootstrap.cmd
@@ -1,4 +1,4 @@
 echo "salt step"
-salt-run winrepo.update_git_repos
-salt-call --local chocolatey.install jdk8.install
-salt-call --local chocolatey.install vscode.install
+salt-call -G 'os:Windows' pkg.refresh_db
+salt-call --local chocolatey.install jdk8
+salt-call --local chocolatey.install vscode
