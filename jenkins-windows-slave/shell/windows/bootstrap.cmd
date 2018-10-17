260fe9a29117c9b682d1f27d948ebefa0a0c7f98
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-17 13:31:04 +0100

    Added bootstrap folder v18

--- jenkins-windows-slave/shell/windows/bootstrap.cmd
+++ jenkins-windows-slave/shell/windows/bootstrap.cmd
@@ -1,9 +1,9 @@
-echo "salt step"
+echo "salt bootstrap start"
 
-echo "#grains:
-  roles:
-    - misprofile1" > C:\salt\conf\minion.d\minion.conf
-    
-salt-call pkg.refresh_db
-salt-call --local chocolatey.install jdk8
-salt-call --local chocolatey.install vscode
+echo "grains:" > C:\salt\conf\minion.d\minion.conf
+echo "  roles:" >> C:\salt\conf\minion.d\minion.conf
+echo "    - misprofile1" >> C:\salt\conf\minion.d\minion.conf
+
+salt-call pkg.refresh_db && salt-call --local chocolatey.install jdk8 && salt-call --local chocolatey.install vscode
+
+echo "salt bootstrap end"
