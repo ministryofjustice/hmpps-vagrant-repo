293a828e450e8f25a453553957f0a1984ff61223
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-17 13:09:15 +0100

    Added bootstrap folder v16

--- jenkins-windows-slave/shell/centos/bootstrap-saltmaster.sh
+++ jenkins-windows-slave/shell/centos/bootstrap-saltmaster.sh
@@ -2,6 +2,10 @@
 
 echo '### SALTMASTER SETUP'
 
+mkdir /data/salt
+
+ln -sf /srv/salt /data/salt
+
 yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm -y
 
 yum install -y salt-master salt-minion git
@@ -19,4 +23,4 @@ yum install python-pygit2 -y
 salt-run winrepo.genrepo
 salt-run winrepo.update_git_repos
 
-salt -G 'os:windows' pkg.refresh_db
+salt -G 'os:Windows' pkg.refresh_db
