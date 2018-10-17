edbd9c12fbc7906c3f73d90614ade193d29f85c1
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-17 14:14:40 +0100

    Added bootstrap folder v20

--- jenkins-windows-slave/shell/windows/bootstrap.cmd
+++ jenkins-windows-slave/shell/windows/bootstrap.cmd
@@ -1,5 +1,5 @@
 echo "salt bootstrap start"
 
-salt-call pkg.refresh_db && salt-call --local chocolatey.install jdk8 && salt-call --local chocolatey.install vscode
+salt-call pkg.refresh_db && salt-call --local chocolatey.install vscode
 
 echo "salt bootstrap end"
