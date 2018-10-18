3c68a8c746810f339ef43de8f1fd5d488199cba4
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-18 14:02:01 +0100

    Added bootstrap folder v31

--- jenkins-windows-slave/shell/centos/bootstrap-saltmaster.sh
+++ jenkins-windows-slave/shell/centos/bootstrap-saltmaster.sh
@@ -14,15 +14,21 @@ systemctl enable salt-master salt-minion
 systemctl start salt-master salt-minion
 
 echo 'auto_accept: True
-file_roots:
-  base:
-    - /srv/salt/states
-pillar_roots:
-  base:
-    - /srv/salt/pillars     
 default_top: base
 state_top_saltenv: base
 top_file_merging_strategy: same
+
+fileserver_backend:
+  - gitfs
+
+gitfs_base: develop
+gitfs_remotes:
+  - https://github.com/ministryofjustice/hmpps-mis-saltstack-states.git
+
+git_pillar_base: develop
+ext_pillar:
+  - git:
+    - develop https://github.com/ministryofjustice/hmpps-mis-saltstack-pillars.git
 ' > /etc/salt/master.d/master.conf
 
 systemctl restart salt-master
