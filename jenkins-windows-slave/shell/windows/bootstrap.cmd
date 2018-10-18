8a445688a98dd9bd7df9bbfe81ab72c6a331679c
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-18 12:17:26 +0100

    Added bootstrap folder v26

--- jenkins-windows-slave/shell/windows/bootstrap.cmd
+++ jenkins-windows-slave/shell/windows/bootstrap.cmd
@@ -1,9 +1,15 @@
 echo "salt bootstrap start"
 
+rmdir /s /q c:\salt\pillars
+
+rmdir /s /q c:\salt\states
+
 git clone -b issue-1 https://github.com/ministryofjustice/hmpps-mis-saltstack-pillars.git c:\salt\pillars
 
 git clone -b issue-1 https://github.com/ministryofjustice/hmpps-mis-saltstack-states.git c:\salt\states
 
-salt-call pkg.refresh_db && salt-call state.highstate
+copy c:\salt\states\bootstrap\fileroots.conf C:\salt\conf\minion.d\fileroots.conf
+
+salt-call --local state.highstate
 
 echo "salt bootstrap end"
