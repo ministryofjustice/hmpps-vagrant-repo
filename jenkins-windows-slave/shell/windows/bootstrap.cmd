3243417806db76b234c7c3ff0181f3f4b8d8ccfc
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-18 13:02:30 +0100

    Added bootstrap folder v30

--- jenkins-windows-slave/shell/windows/bootstrap.cmd
+++ jenkins-windows-slave/shell/windows/bootstrap.cmd
@@ -1,12 +1,12 @@
 echo "salt bootstrap start"
 
-rmdir /s /q c:\salt\pillars
-
 rmdir /s /q c:\salt\states
 
 git clone -b issue-1 https://github.com/ministryofjustice/hmpps-mis-saltstack-states.git c:\salt\states
 
 copy c:\salt\states\bootstrap\minion.conf C:\salt\conf\minion.d\minion.conf 
+
+rmdir /s /q c:\salt\states
 
 net stop salt-minion
 
