cd35df3d03b9f975f3aa1075fff6edb3804ae633
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-18 12:04:04 +0100

    Added bootstrap folder v25

--- jenkins-windows-slave/shell/windows/bootstrap.cmd
+++ jenkins-windows-slave/shell/windows/bootstrap.cmd
@@ -1,5 +1,9 @@
 echo "salt bootstrap start"
 
+git clone -b issue-1 https://github.com/ministryofjustice/hmpps-mis-saltstack-pillars.git c:\salt\pillars
+
+git clone -b issue-1 https://github.com/ministryofjustice/hmpps-mis-saltstack-states.git c:\salt\states
+
 salt-call pkg.refresh_db && salt-call state.highstate
 
 echo "salt bootstrap end"
