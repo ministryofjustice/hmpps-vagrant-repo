eb06b726b555dfeb125fa84075d5c596bdca783d
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-10-19 13:02:59 +0100

    Added bootstrap folder v38

---
+++ jenkins-windows-slave/shell/windows/bootstrap.ps1
@@ -0,0 +1,31 @@
+$ChocoInstallPath = "$env:SystemDrive\ProgramData\Chocolatey\bin"
+
+if (!(Test-Path $ChocoInstallPath)) {
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# # Host entry
+# Add-Content -Path C:\Windows\System32\Drivers\etc\hosts -Value "172.26.243.12 salt"
+
+#---- TEMPORARY ---
+Disable-UAC
+
+#--- Python ---
+choco install python -y
+
+#--- awscli ---
+choco install awscli -y
+
+refreshenv
+
+# #--- Salt Minion ----
+# choco install saltminion -y
+
+#--- Git ----
+choco install git -y
+
+refreshenv
+
+Invoke-Expression ((new-object net.webclient).DownloadString('https://www.devopslibrary.com/bootstrap-salt.ps1'))
+
+Enable-UAC
